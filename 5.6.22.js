15a7c57da975513a49e16d04543426c7ea6340ef
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-07 10:34:46 -0500

    2nd prob of the day

--- 5.6.22.js
+++ 5.6.22.js
@@ -7,3 +7,16 @@
 function squareSum(numbers){
   return numbers.map(x => x**2).reduce((a, c) => a + c, 0)
 }
+
+
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+
+function maps(x){
+  return x.map(x => x * 2)
+}
