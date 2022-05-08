cb0283763187ca3766f37636d3c59c7994feb867
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-08 00:50:01 -0500

    Multiply each value of array together

--- 5.8.22.js
+++ 5.8.22.js
@@ -34,3 +34,12 @@ function sum (numbers) {
 function check(a, x) {
   return a.includes(x)
 }
+
+
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x){
+  return x.reduce((acc, c) => acc * c, 1)
+}
