5dac2a99f6289fde9371ca599475c54419f0a6cc
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-08 13:16:14 -0500

    Removing every second element in array

--- 5.8.22.js
+++ 5.8.22.js
@@ -131,3 +131,20 @@ function firstNonConsecutive (arr) {
     }
     return null
 }
+
+
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+//This works because we start by removing index 1, then everything shifts down indexs, then we remove index 2, so on and so forth, the fact that the index always moves down one makes this work.
+
+function removeEveryOther(arr){
+  for(let i=1; i < arr.length; i++){
+    arr.splice(i, 1)
+  } return arr
+}
