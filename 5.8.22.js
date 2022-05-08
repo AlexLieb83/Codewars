ec879992d3d1e45643e3286d5fd5e715370f39a1
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-08 14:19:35 -0500

    Sort array, grab first element, then return with *** between each letter

--- 5.8.22.js
+++ 5.8.22.js
@@ -148,3 +148,18 @@ function removeEveryOther(arr){
     arr.splice(i, 1)
   } return arr
 }
+
+
+You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+
+//first sort s
+//then grab the first element in the array
+//split it up by letter, into an array
+//then join them back together with ***
+function twoSort(s) {
+  return s.sort()[0].split('').join('***')
+}
