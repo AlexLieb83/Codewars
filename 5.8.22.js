4fe047a45e6c23f951f1757a9a2dd2db81a79353
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-08 14:20:09 -0500

    Comments

--- 5.8.22.js
+++ 5.8.22.js
@@ -150,11 +150,11 @@ function removeEveryOther(arr){
 }
 
 
-You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+// You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
 
-The returned value must be a string, and have "***" between each of its letters.
+// The returned value must be a string, and have "***" between each of its letters.
 
-You should not remove or add elements from/to the array.
+// You should not remove or add elements from/to the array.
 
 //first sort s
 //then grab the first element in the array
