3e8cd352e7cc0322936dad1dfb126b4c832c53fe
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-08 00:22:10 -0500

    If x is in array return true, else return false

--- 5.8.22.js
+++ 5.8.22.js
@@ -23,3 +23,14 @@
 function sum (numbers) {
   return numbers.reduce((acc, c) => acc + c, 0)
 };
+
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+function check(a, x) {
+  return a.includes(x)
+}
