014435cf07ef54ec71817147a4ed0ef5c1fe57cf
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-09 14:04:00 -0500

    Swap Values function prob

--- 5.9.22.js
+++ 5.9.22.js
@@ -56,3 +56,43 @@ const arr = N => {
 function printArray(array){
   return array.join()
 }
+
+
+
+// Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+
+// Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+
+// [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+//    7      6      5      4      3            2      1
+
+// If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+// Note: there will always be exactly one wolf in the array.
+// Examples
+
+// Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+// Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+
+// Input: ["sheep", "sheep", "wolf"]
+// Output: "Pls go away and stop eating my sheep"
+
+//first we make the wolf variable, which is reversing the queue array, so we can just grab the index of 'wolf'
+//then we check, if wolf is in the index of 0, then it will be next to us and will eat us, if not, then return it's index, which will be the same as the sheep number in line
+function warnTheSheep(queue) {
+  let wolf = queue.reverse().indexOf('wolf')
+  if(wolf === 0){
+    return 'Pls go away and stop eating my sheep'
+  } else {
+    return `Oi! Sheep number ${wolf}! You are about to be eaten by a wolf!`
+  }
+}
+
+
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+
+function swapValues(arr) {
+  return arr.reverse()
+}
