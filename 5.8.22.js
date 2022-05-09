0de7d13594589313927661705d08fb9ca6e3ad7a
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-08 19:32:58 -0500

    Good/Bad Ideas prob

--- 5.8.22.js
+++ 5.8.22.js
@@ -163,3 +163,23 @@ function removeEveryOther(arr){
 function twoSort(s) {
   return s.sort()[0].split('').join('***')
 }
+
+
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+//make two arrays that hold good and bad values respectively. 
+//then set up if statements to return depending on counts
+function well(x){
+  let good = x.filter(word => word === 'good')
+  let bad = x.filter(word => word === 'bad')
+  
+  if(good.length > 2){
+    return 'I smell a series!'
+  } else if(good.length >= 1){
+    return 'Publish!'
+  } else{
+    return 'Fail!'
+  }
+}
