c56f3e6ede01aa339662e8b40a7d9f7caa4db668
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-09 11:35:35 -0500

    Filling an array, from 0 to N

--- 5.9.22.js
+++ 5.9.22.js
@@ -22,3 +22,24 @@ function gooseFilter (birds) {
   let noGeese = birds.filter(el => !geese.includes(el))
   return noGeese
 };
+
+
+// We want an array, but not just any old array, an array with contents!
+
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+
+// For example, the following code will result in an array containing the numbers 0 to 4:
+
+// arr(5) // => [0,1,2,3,4]
+
+// Note: The parameter is optional. So you have to give it a default value.
+
+//make arr an empty string inside of the function
+//then run a for loop to run up to N, not including N, and push each number into the arr array. This will start from 0 and count up to N
+const arr = N => {
+  const arr =[]
+  
+  for(let i = 0; i < N; i++){
+    arr.push(i)
+  } return arr
+} 
