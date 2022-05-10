95504edb84bafdd7767b3de8dbaf93e7a3bea1ce
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-10 15:26:45 -0500

    Slice prob

--- 5.10.22.js
+++ 5.10.22.js
@@ -181,3 +181,20 @@ var ArrowFunc = function(arr) {
 function toCsvText(array) {
  return array.join('\n')
 } 
+
+
+// Write a function to get the first elements of asequence. Passing a parameter n (default=1) will return the first n elements of the sequence.
+
+// If n == 0 return an empty sequence []
+// Examples
+
+// var arr = ['a', 'b', 'c', 'd', 'e'];
+// first(arr) //=> ['a'];
+// first(arr, 2) //=> ['a', 'b']
+// first(arr, 3) //=> ['a', 'b', 'c'];
+// first(arr, 0) //=> [];
+
+//we set n=1 because the default is 1, then we are just returning a slice of the arr from the beginning to whatever is called for.
+function first(arr, n=1) {
+  return arr.slice(0,n);
+}
