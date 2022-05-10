b0f828186f60308c15cae66d44e7254d7b556139
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-10 14:20:57 -0500

    Duck duck goose problem, return goose's name

--- 5.10.22.js
+++ 5.10.22.js
@@ -80,3 +80,25 @@ function sumOfDifferences(arr) {
     sum += diff
   } return sum
 }
+
+
+// The objective of Duck, duck, goose is to walk in a circle, tapping on each player's head until one is chosen.
+
+// Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+
+// Example:
+
+// duck_duck_goose([a, b, c, d], 1) should return a.name
+// duck_duck_goose([a, b, c, d], 5) should return a.name
+// duck_duck_goose([a, b, c, d], 4) should return d.name
+
+// // PHP only
+// duck_duck_goose([$a, $b, $c, $d], 1); // => $a["name"]
+// duck_duck_goose([$a, $b, $c, $d], 5); // => $a["name"]
+// duck_duck_goose([$a, $b, $c, $d], 4); // => $d["name"]
+
+//goose - 1 will return the index of the chosen player. then take the remainder - taking the remainder will allow us to always return the proper place, even if the goose number laps the number of players - of the index / the length of the array. then grab that name and return it
+function duckDuckGoose(players, goose) {
+  return players[(goose-1)%players.length].name
+}
+
