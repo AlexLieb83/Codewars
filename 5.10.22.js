6c5b42ebdb734f75a72c2f4d13e6369bd65ad2c6
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-10 15:10:31 -0500

    CSV representation of array prob

--- 5.10.22.js
+++ 5.10.22.js
@@ -157,3 +157,27 @@ function all( arr, fun ){
 var ArrowFunc = function(arr) {
   return arr.map(x => String.fromCharCode(x)).join(''); 
 }
+
+
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+// Example:
+
+// input:
+//    [[ 0, 1, 2, 3, 4 ],
+//     [ 10,11,12,13,14 ],
+//     [ 20,21,22,23,24 ],
+//     [ 30,31,32,33,34 ]] 
+    
+// output:
+//      '0,1,2,3,4\n'
+//     +'10,11,12,13,14\n'
+//     +'20,21,22,23,24\n'
+//     +'30,31,32,33,34'
+
+// Array's length > 2.
+
+//return the array with \n at the end of each line
+function toCsvText(array) {
+ return array.join('\n')
+} 
