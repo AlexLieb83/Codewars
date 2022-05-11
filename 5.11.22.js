9e686b97c7f42c21c7d74ec8bc9ec6bfcf940935
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-11 14:57:44 -0500

    Vowel Count prob

--- 5.11.22.js
+++ 5.11.22.js
@@ -226,3 +226,23 @@ function defineSuit(card) {
   if(card.includes('♥')) return 'hearts'
   if(card.includes('♠')) return 'spades'
 }
+
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+//def our vowels and our count to 0, then run a for loop over the string, if a vowel exists at an index, then add 1 to our count, return the count
+function getCount(str) {
+  let vowels = 'aeiou';
+  let vowelsCount = 0;
+      
+   for(let i = 0; i < str.length ; i++) {
+      if (vowels.indexOf(str[i]) !== -1) {
+        vowelsCount += 1;
+    }
+  }
+   return vowelsCount
+}
