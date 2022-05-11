62ff04941674508073e43fe9821d47bf46378649
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-10 21:14:52 -0500

    Remove First and Last Character of String

--- 5.10.22.js
+++ 5.10.22.js
@@ -198,3 +198,15 @@ function toCsvText(array) {
 function first(arr, n=1) {
   return arr.slice(0,n);
 }
+
+
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+function removeChar(str){
+   let arr = str.split('')
+   arr.shift()
+   arr.pop()
+  
+   return arr.join('')
+ };
+ 
