ddd9532b305f2729ad61354751c55938c4b6ddf9
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-05-10 21:19:32 -0500

    Returning Strings prob

--- 5.10.22.js
+++ 5.10.22.js
@@ -210,3 +210,11 @@ function removeChar(str){
    return arr.join('')
  };
  
+
+//  Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
