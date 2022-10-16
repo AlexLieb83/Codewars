408455fdecc14364088f0950a88ba7c0509e15bd
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-10-16 18:50:10 -0500

    codewars for 10.16.22

---
+++ findNearestSquareNum.js
@@ -0,0 +1,8 @@
+function nearestSq(n) {
+  return Math.pow(Math.round(Math.sqrt(n)), 2);
+}
+
+//p - number
+//r - clostest square number to n
+//ps - sqrt n, then round, and ^2
+//10 -> 3.16227 -> 3 -> 9
