d30a53bc5fcf6ff1d20d2dbdbd65d6cc55e9ee70
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-11-12 19:02:07 -0600

    codewars for 11.12.22

--- happyNumbers.js
+++ happyNumbers.js
@@ -5,7 +5,7 @@
 // Write a function that takes n as parameter and return true if and only if n is an happy number, false otherwise.
 
 // Examples
-// For example number 7 is happy because after a number of steps the computed sequence ends up with a 1: 7, 49, 97, 130, 10, 1 
+// For example number 7 is happy because after a number of steps the computed sequence ends up with a 1: 7, 49, 97, 130, 10, 1
 
 // While 3 is not, and would give us an infinite sequence: 3, 9, 81, 65, 61, 37, 58, 89, 145, 42, 20, 4, 16, 37, 58, 89, 145, 42, 20, 4, 16, 37, ...
 
