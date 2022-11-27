1c5f0c315c07adccfb65c3ef7033b3497642bdca
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-11-26 22:07:34 -0600

    codewars for 11.26.22

--- urlParser.js
+++ urlParser.js
@@ -15,3 +15,14 @@ function domainName(url) {
 console.log(domainName("http://github.com/carbonfive/raygun"), "github");
 console.log(domainName("http://www.zombie-bites.com"), "zombie-bites");
 console.log(domainName("https://www.cnet.com"), "cnet");
+
+// function domainName(url){
+//   let arr = url.replace('http://', '').replace('https://', '').replace('www.', '').split('.')
+
+//   return arr[0]
+// }
+
+//p - url string
+//r - domain name from url string, string
+//ps - remove everything before the domain name -- url.replace https http www.
+//then split at . and return arr[0]
