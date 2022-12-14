2b84e12abef24460b4236d05af8fb66922422c44
alexlieb <alexliebhaber1992@gmail.com>
2022-12-14 17:19:40 -0600

    leetcode for 12.14.22

--- ransomNote.js
+++ ransomNote.js
@@ -10,46 +10,70 @@ const magazine =
 //then check if we have word from ransom note, if not, return false, if so, check that we have the same or more value in magazine, if not, return false
 //if we make it out of our ransome note loop, then we have the correct words and enough words in magazine, so return true
 
-function ransomNote(str, magazine) {
-  let ransomWords = {};
-  let magazineWords = {};
+function ransomNote(note, magazine) {
+  const noteWords = note.split(" ");
+  const magazineWords = magazine.split(" ");
+  const magazineHash = {};
 
-  let ransomArr = str.split(" ");
-  let magazineArr = magazine.split(" ");
-
-  // adding ransom words and values to map
-  for (let i = 0; i < ransomArr.length; i++) {
-    let value = ransomArr[i];
-    if (ransomWords[value] !== undefined) {
-      ransomWords[value]++;
-    } else {
-      ransomWords[value] = 1;
-    }
-  }
-  // adding magazine words and values to map
-  for (let i = 0; i < magazineArr.length; i++) {
-    let value = magazineArr[i];
-    if (magazineWords[value] !== undefined) {
-      magazineWords[value]++;
-    } else {
-      magazineWords[value] = 1;
-    }
+  //loop over magazineWords, add words to hash and increment their value or set their value to 1
+  for (const word of magazineWords) {
+    magazineHash[word] = magazineHash[word] + 1 || 1;
   }
 
-  // now check that enough of each word from ransom note exists in magazine
-  for (const key in ransomWords) {
-    // if magazineWords has the same key as ransomWords, we need to make sure we have enough count
-    if (!magazineWords.hasOwnProperty(key)) {
+  //loop over noteWords, if they exist in our Hashmap, decriment value by 1
+  for (const word of noteWords) {
+    if (magazineHash[word]) {
+      magazineHash[word]--;
+      // if the word value is less than 0 in magazine, then we have ran out of that word to cut out, return false
+      if (magazineHash[word] < 0) return false;
+      //if the word doesn't exist then return false, it doesn't exist in our magazine
+    } else {
       return false;
-    } else {
-      if (magazineWords[key] < ransomWords[key]) {
-        return false;
-      }
     }
   }
-  // We will get to here if we have enough words from magazine for our ransom note
   return true;
 }
+
+// function ransomNote(str, magazine) {
+//   let ransomWords = {};
+//   let magazineWords = {};
+
+//   let ransomArr = str.split(" ");
+//   let magazineArr = magazine.split(" ");
+
+//   // adding ransom words and values to map
+//   for (let i = 0; i < ransomArr.length; i++) {
+//     let value = ransomArr[i];
+//     if (ransomWords[value] !== undefined) {
+//       ransomWords[value]++;
+//     } else {
+//       ransomWords[value] = 1;
+//     }
+//   }
+//   // adding magazine words and values to map
+//   for (let i = 0; i < magazineArr.length; i++) {
+//     let value = magazineArr[i];
+//     if (magazineWords[value] !== undefined) {
+//       magazineWords[value]++;
+//     } else {
+//       magazineWords[value] = 1;
+//     }
+//   }
+
+//   // now check that enough of each word from ransom note exists in magazine
+//   for (const key in ransomWords) {
+//     // if magazineWords has the same key as ransomWords, we need to make sure we have enough count
+//     if (!magazineWords.hasOwnProperty(key)) {
+//       return false;
+//     } else {
+//       if (magazineWords[key] < ransomWords[key]) {
+//         return false;
+//       }
+//     }
+//   }
+//   // We will get to here if we have enough words from magazine for our ransom note
+//   return true;
+// }
 
 // TESTS
 // describe("Ransom Note", () => {
