616387f75963ff91d096f9fcb540c47a42ea7f2f
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-12-15 20:56:49 -0600

    added comments

--- twoSumAlt.js
+++ twoSumAlt.js
@@ -1,14 +1,21 @@
 // Given an array of numbers, return all pairs that add up to a given sum. The numbers can be used more than once.
 
 function twoSum(arr, target) {
+  //pairs will hold our return values
   const pairs = [];
+  //holds are number values to match with
   const map = {};
 
+  //loop over the given array
   for (const num1 of arr) {
+    //we are looking for a number that matches are target - number we are looking at, compmliment number
     const num2 = target - num1;
+    // check our map for our compliment number
     if (map[num2]) {
+      // if we find it, push the pair of numbers to our return arr
       pairs.push([num1, num2]);
     } else {
+      //else store that number in our map
       map[num1] = 1;
     }
   }
