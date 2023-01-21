168c7872ad6e7cca4b93006d94bdacac88f210e1
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-01-21 09:17:04 -0600

    leetcode 1.20.23

--- isAllTrue.js
+++ isAllTrue.js
@@ -3,7 +3,7 @@
 //     a sequence
 //     a function (function pointer in C)
 
-// and returns true if the function in the params returns true for every element in the sequence. Otherwise, it should return false. If the sequence is empty, it should return true, since technically nothing failed the test.
+// and returns true if the function in the params returns true for every element in the sequence. Otherwise, it should return false. If the sequence is empty, it should return true, since technically nothing failed the test
 // Example
 
 // all((1, 2, 3, 4, 5), greater_than_9) -> false
