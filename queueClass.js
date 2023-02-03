8d2888f8a4bbd76a03ab3afcaf293e054e1b73a1
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-02-03 17:07:49 -0600

    finished queueClass codewars from 2.3.23

--- queueClass.js
+++ queueClass.js
@@ -3,27 +3,21 @@
 class Queue {
   // implement your Queue constructor here
   constructor() {
-    this.items = {};
-    this.head = 0;
-    this.tail = 0;
+    this.list = [];
   }
 }
 
 Queue.prototype.enqueue = function (item) {
   // add item to the queue
-  this.items[this.tail] = item;
-  this.tail++;
+  this.list.push(item);
 };
 
 Queue.prototype.dequeue = function () {
   // remove item from the front of the queue and return its value
-  const item = this.items[this.head];
-  delete this.items[this.head];
-  this.head++;
-  return item;
+  return this.list.shift();
 };
 
 Queue.prototype.size = function () {
   // return number of items in queue so far
-  return this.tail;
+  return this.list.length;
 };
