194aeada4f6c58f6e0237befe94d136d82696fa1
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-02-13 17:14:23 -0600

    learning more about linked lists

--- linkedList.js
+++ linkedList.js
@@ -1,4 +1,8 @@
 // Create a Doubly Linked List Class
+// arrays take up contiguous memory, which is great for speed, but adding and removing things can slow things down.
+// linked lists do not have to be contiguous, they point to what is next or previous - this helps with adding and deleting.
+// when dealing with small amounts of data, link lists are the way to go.
+// commonly used on problems that have order, and need to add and remove things. Music player, flight schedules
 
 class LinkedList {
   constructor() {
