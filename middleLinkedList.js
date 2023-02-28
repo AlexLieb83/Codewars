0f5aba4a34c148fb89f456f4e44a978ff3df7b95
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-02-28 14:59:23 -0600

    leetcode for 2.28.23

--- middleLinkedList.js
+++ middleLinkedList.js
@@ -14,8 +14,7 @@
  * @return {ListNode}
  */
 var middleNode = function (head) {
-  let fast = head;
-  let slow = head;
+  let fast = (slow = head);
 
   while (fast && fast.next) {
     fast = fast.next.next;
