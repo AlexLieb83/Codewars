877637edbefee53f73a7e6b814aa4a195dcbc87c
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-03-01 12:17:29 -0600

    codewars for 3.1.23

--- middleLinkedList.js
+++ middleLinkedList.js
@@ -27,7 +27,7 @@ var middleNode = function (head) {
 
 // Input: head = [1,2,3,4,5]
 // Output: [3,4,5]
-// Explanation: The middle node of the list is node 3.
+// Explanation: The middle node of the list is node 3
 // Example 2:
 
 // Input: head = [1,2,3,4,5,6]
